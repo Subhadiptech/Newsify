664a4c0f031da85881f27f8bb017e40b060120fa
Subhadiptech <2020ugee055@nitjsr.ac.in>
2021-12-27 22:14:12 +0530

    instanciated viewModel and repositories in newsActivity

--- app/src/main/java/com/ersubhadip/newsify/NewsActivity.kt
+++ app/src/main/java/com/ersubhadip/newsify/NewsActivity.kt
@@ -2,14 +2,31 @@ package com.ersubhadip.newsify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.ersubhadip.newsify.Database.ArticleDatabase
+import com.ersubhadip.newsify.databinding.ActivityNewsBinding
+import com.ersubhadip.newsify.repository.NewsRepository
+import com.ersubhadip.newsify.viewmodel.NewsViewModel
+import com.ersubhadip.newsify.viewmodel.NewsViewModelProviderFactory
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NewsActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityNewsBinding
+    lateinit var newsViewModel:NewsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val newsRepository = NewsRepository(ArticleDatabase.getDatabase(this))
+        val newsViewModelFactory = NewsViewModelProviderFactory(newsRepository)
+        newsViewModel = ViewModelProvider(this,newsViewModelFactory).get(newsViewModel::class.java)
 
-//        findViewById<BottomNavigationView>(R.id.bottomNav).setupWithNavController()
+//        binding.bottomNav.setupWithNavController()
+
+
     }
 }
